a6ecd5c3df6af789c28f2237d42eafc62271d4ad
aniii6099 <ch4random@gmail.com>
2026-03-05 12:36:44 +0530

    fix of backend7

--- backend/main.py
+++ backend/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://arch-ai-chi-lac.vercel.app",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
